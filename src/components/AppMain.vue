118493022384d647a59ac562006ecc6455785459
GabrieleGrossi <grossigabriele98@gmail.com>
2023-06-21 11:50:47 +0200

    Create and import main and header in App

---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>
+            I'm the main
+        </h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'AppMain',
+}
+</script>
+<style>
+    
+</style>
