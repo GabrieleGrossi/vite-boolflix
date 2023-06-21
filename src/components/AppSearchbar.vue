8d1f6488ca619433e38d2fffa6211eb7afff701e
GabrieleGrossi <grossigabriele98@gmail.com>
2023-06-21 14:12:02 +0200

    very ugly searchbar

--- src/components/AppSearchbar.vue
+++ src/components/AppSearchbar.vue
@@ -1,13 +1,19 @@
 <template>
     <div>
-        <h2>
-            I'm the searchbar
-        </h2>
+        <input type="text" v-model="store.searchBar">
+        <button>Search film</button>
     </div>
 </template>
 <script>
+import { store } from '../store';
 export default {
     name: 'AppSearchbar',
+
+    data(){
+        return{
+            store,
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
