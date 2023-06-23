3a8c5b5599acffdd90f1f4247644e2165fb35f28
GabrieleGrossi <grossigabriele98@gmail.com>
2023-06-23 15:23:36 +0200

    Better searchbar

--- src/components/AppSearchbar.vue
+++ src/components/AppSearchbar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="searchbar">
         <i class="fa-solid fa-magnifying-glass icon"></i>
-        <input type="text" v-model="researchText" @keyup.enter="searchMovie(researchText)">
+        <input type="text" v-model="researchText" @keyup.enter="searchMovie(researchText)" placeholder="Search a film/serie tv">
         <button @click="searchMovie(researchText), researchText=''">Search film</button>
     </div>
 </template>
@@ -44,5 +44,15 @@ export default {
     .icon{
         color: white;
         margin-right: 1.5rem;
+    }
+    input{
+        height: 35px;
+        border-radius: 15px;
+        padding: 0.5rem;
+    }
+    button{
+        padding: 0.5rem;
+        border-radius: 15px;
+        margin-left: 1rem;
     }
 </style>
